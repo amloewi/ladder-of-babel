a63aca59325fa2a0017c0fd42a87b13d572beb8a
Alex Loewi <amloewi@gmail.com>
2020-10-18 17:35:17 -0400

    Navbar, About, default text. Good!
    Next up, hide on click

--- src/App.js
+++ src/App.js
@@ -1,16 +1,24 @@
-// import React, { useState, useEffect } from 'react';
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
-import { cedict } from "./small.js";
+import { cedict } from "./medium.js"; // do one for medium w ... 50k
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css'; // This was necessary; ugh
-import { Container, Row, Col } from 'react-bootstrap';
+import { Container, Row, Col, Navbar, Button, Nav, Modal } from 'react-bootstrap';
 
 
 class Translation extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      translation: []
+      translation: [{"token":"在", "pinyin":"[zai4]", "definition": "/(located) at/(to be) in/to exist/in the middle of doing sth/(indicating an action in progress)/"},
+      {"token":"此", "pinyin":"[ci3]", "definition": "/this/these/"},
+      {"token":"粘贴", "pinyin":"[zhan1 tie1]", "definition":"/to stick/to affix/to adhere/to paste (as in 'copy and paste')/Taiwan pr. [nian2 tie1]/also written 黏貼|黏贴[nian2 tie1]/"},
+      {"token":"你", "pinyin":"[ni3]", "definition": "/you (informal, as opposed to courteous 您[nin2])/"},
+      {"token":"自己", "pinyin":"[zi4 ji3]", "definition": "/oneself/one's own/"},
+      {"token":"的", "pinyin":"[de5]", "definition": "/of/~'s (possessive particle)/(used after an attribute)/(used to form a nominal expression)/(used at the end of a declarative sentence for emphasis)/"},
+      {"token":"内容", "pinyin":"[nei4 rong2]", "definition": "/content/substance/details/CL:個|个[ge4],項|项[xiang4]/"},
+      {"token":"!", "pinyin":"", "definition": ""}
+    ],
+      text: "在此粘贴你自己的内容!"
     };
     // Would prefer the less insane syntax for this
     this.handleInputChange = this.handleInputChange.bind(this)
@@ -18,14 +26,13 @@ class Translation extends React.Component {
 
   handleInputChange(event) {
     this.setState({
-        translation: this.translate(event.target.value)
+      text: event.target.value,
+      translation: this.translate(event.target.value),
+      show: this.translation.map (() => true) // OR have it be a property of the word, probably; but, onclick?
     })
   }
 
   translate(text) { 
-    // This is super sloppy for all sorts of reasons; do need the chinese punctuation though
-    // const punctuation = ["，", "。", "「", "」", "、", "‧", ".", ";", " ", "\t"]
-
     const translation = []
     let remaining_characters = text
     while (remaining_characters.length > 0) {
@@ -39,12 +46,11 @@ class Translation extends React.Component {
             } else {  // candidate is NOT in the dictionary
                 if (best_candidate != null) {  // at least the first character was recognized
                   const line = String(cedict[best_candidate])
-                  const pinyin = (line.split(/](.+)/)[0]) + "]"
-                  const definition = (line.split(/](.+)/)[1]).slice(1) // I only want to split on the FIRST; 
+                  const pinyin = (line.split(/](.+)/)[0]) + "]" // I only want to split on the FIRST ']'; 
+                  const definition = (line.split(/](.+)/)[1]).slice(1) 
                   translation.push({"token": best_candidate, "pinyin": pinyin, "definition": definition})
                 } else {
                     const token = remaining_characters[0]
-                    // const definition = token in punctuation ? "" : "??"
                     translation.push({"token": token, "pinyin": "", "definition": ""})
                 }
                 if (word_boundary > 1) {
@@ -65,11 +71,14 @@ class Translation extends React.Component {
         <input
             type="text"
             onChange={ this.handleInputChange }
+            value={ this.state.text }
         />
-        <br></br>
+        <p></p>
+        {/* I should presumably make this into a component with the behavior I want */}
+        {/* <Word word={word} key={ix} /> */}
         {this.state.translation.map((word, ix) =>
           <Row key={ix} style={{textAlign: "left"}}>
-            <Col md={2}>{word["token"]}</Col> 
+            <Col md={2}>{word["token"]}</Col>
             <Col md={2}>{word["pinyin"]}</Col>
             <Col md={8}>{word["definition"]}</Col>
           </Row>
@@ -79,11 +88,54 @@ class Translation extends React.Component {
   }
 }
 
+function About() {
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+  return (
+    <>
+      <Button variant="primary" onClick={handleShow}>
+        About
+      </Button>
+
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>About Ladder of Babel</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          Ladder of Babel is a tool for beginner and intermediate students of Chinese that makes any (simplified) text accessible by solving a 
+          serious problem with most current tools.<p></p>
+          Tools such as the Google Translate browser plugin, or the similar Mate Translate, are perfect for translating a single word at a time. They are 
+          clearly an enormous improvement over having to search through a paper dictionary. However, they are still best for people who only need to pick out
+          words one by one, whereas
+          most Chinese language learners both need to translate <b>most</b> of the words - and, probably don't even know which characters 
+           <b> form</b> a single word.<p></p>
+           Ladder of Babel solves both of these problems by pre-identifying which characters form words, and giving definitions for everything, at once - 
+           making the reading experience as seamless and fluent as possible even for early stage students, and opening up the entire world of online Chinese
+           text for easy access, and also productive study.
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="primary" onClick={handleClose}>
+            Close
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </>
+  );
+}
+
 function App() {
   return (
     <div className="App">
-      <h2>Ladder of Babel</h2>
-      Paste simplified Chinese here for a full word-by-word translation
+      <Navbar bg="light" expand="lg">
+        <Navbar.Brand href="#home">Ladder of Babel</Navbar.Brand>
+        <Nav className="mr-auto"></Nav> {/* Using this to push the rest to the right */}
+        <About />
+        <Nav.Link href="http://dovecoteinstitute.org">Dovecote Institute</Nav.Link>
+      </Navbar>
+
       <p></p>
       <Translation />
       <hr></hr>
@@ -97,9 +149,9 @@ function App() {
 
 export default App;
 
-//         text="“我有两张自拍，一张很丑，另一张还是很丑”（“在我的后园，可以看见墙外有两株树，一株是枣树，还有一株也是枣树。”——迅哥原文）
+// “我有两张自拍，一张很丑，另一张还是很丑”（“在我的后园，可以看见墙外有两株树，一株是枣树，还有一株也是枣树。”——迅哥原文）
 // 100年前的鲁迅万万没有预料到，自己在当下的符号是“中文互联网梗王”、“金句界顶流”。
 // 在各种社会热点的留言评论区，在各大平台的鸡汤文学里，鲁迅语录随处可见；无论搭不搭边，“鲁迅曾经说过”就在那里，从不缺席。
 // 北京鲁迅博物馆为此专门建了一个检索系统，在这里，所有“鲁迅说过的话”都可以被验证或者打脸，该系统还不止一次因为查询人数过多而崩溃。
 // 在互联网的迷幻国度里，人人以为自己熟读鲁迅。
-// 人人不知道的是，这个活在100年前的老男孩，早早已将我们读懂、看透。"
+// 人人不知道的是，这个活在100年前的老男孩，早早已将我们读懂、看透。
